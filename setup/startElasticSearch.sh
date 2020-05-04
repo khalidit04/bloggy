955219794025ae493622e80ec5914614bc08c354
Khalid Moin <khalidit04@gmail.com>
2020-05-04 18:47:53 +0200

    Update startElasticSearch.sh

--- setup/startElasticSearch.sh
+++ setup/startElasticSearch.sh
@@ -6,4 +6,6 @@
 
 echo "Running elastic searver.."
 
-sudo docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.0
+## sudo docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.0
+
+docker run  -p 9200:9200 -p 9300:9300 -v es-data:/usr/share/elasticsearch/data  -e "discovery.type=single-node" --name=elasticdb docker.elastic.co/elasticsearch/elasticsearch:7.6.0
